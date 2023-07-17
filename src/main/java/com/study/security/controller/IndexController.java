dbe1fc863bf2e88225c5248ffb6c4a0a869f43af
Hc-5514 <tbd05050@naver.com>
2023-07-17 15:09:11 +0900

    스프링부트 시큐리티 2강 - 시큐리티 설정 권한 설정

--- src/main/java/com/study/security/controller/IndexController.java
+++ src/main/java/com/study/security/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.study.security.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
@@ -11,5 +12,36 @@ public class IndexController {
 		// 머스테치 기본 폴더: src/main/resources/
 		// 뷰리졸버 설정 - 생략가능: templates (prefix), .mustache (suffix)
 		return "index";
+	}
+
+	@GetMapping("/user")
+	public @ResponseBody String user() {
+		return "user";
+	}
+
+	@GetMapping("/admin")
+	public @ResponseBody String admin() {
+		return "admin";
+	}
+
+	@GetMapping("/manager")
+	public @ResponseBody String manager() {
+		return "manager";
+	}
+
+	// SecurityConfig 파일 생성 후 작동 안함.
+	@GetMapping("/login")
+	public @ResponseBody String login() {
+		return "login";
+	}
+
+	@GetMapping("/join")
+	public @ResponseBody String join() {
+		return "join";
+	}
+
+	@GetMapping("/joinProc")
+	public @ResponseBody String joinProc() {
+		return "회원가입 완료됨!";
 	}
 }
