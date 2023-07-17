31fa3c31da0a5c5c5349562bc76ead93affe40a9
Hc-5514 <tbd05050@naver.com>
2023-07-17 14:49:06 +0900

    Init

---
+++ src/main/java/com/study/security/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.study.security.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+	@GetMapping({"", "/"})
+	public String index() {
+		// 머스테치 기본 폴더: src/main/resources/
+		// 뷰리졸버 설정 - 생략가능: templates (prefix), .mustache (suffix)
+		return "index";
+	}
+}
